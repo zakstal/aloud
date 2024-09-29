359f15a9153e30b9b0b2be71fef581d8125ceb33
zak stallings <zakstal@gmail.com>
2024-09-29 18:20:18 -0500

    change files to ts

--- packages/script-to-audio/src/voiceApis/index.js
+++
@@ -1,7 +0,0 @@
-import { textToSpeech as textToSpeechMurph  } from "./murph/murph";
-import { textToSpeechWithTimeStamps as textToSpeechElevent  } from "./elevenLabs/elevenLabs";
-
-export default {
-    murph: textToSpeechMurph,
-    elevenLabs: textToSpeechElevent
-}
