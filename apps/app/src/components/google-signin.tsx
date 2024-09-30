c24da4fee5e0c617f243d8e2c4f9316b016c25d6
zak stallings <zakstal@gmail.com>
2024-09-30 10:12:01 -0500

    defalt for window

--- apps/app/src/components/google-signin.tsx
+++ apps/app/src/components/google-signin.tsx
@@ -3,14 +3,16 @@
 import { createClient } from "@v1/supabase/client";
 import { Button } from "@v1/ui/button";
 
+let window = window || {}
+
 export function GoogleSignin() {
   const supabase = createClient();
-console.log("redirect locaiton",  `${window.location.origin}/dashboard/screen-play/new`)
+console.log("redirect locaiton",  `${window?.location?.origin}/dashboard/screen-play/new`)
   const handleSignin = () => {
     supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/dashboard/screen-play/new`,
+        redirectTo: `${window?.location?.origin}/dashboard/screen-play/new`,
         // redirectTo: `${window.location.origin}/api/auth/callback`,
       },
     });
