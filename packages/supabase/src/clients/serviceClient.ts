9f6fb31b107892f2740e35c0bc2722c3162601ac
zak stallings <zakstal@gmail.com>
2024-10-17 13:24:29 -0500

    change name of anon key

--- packages/supabase/src/clients/serviceClient.ts
+++ packages/supabase/src/clients/serviceClient.ts
@@ -5,6 +5,6 @@ export const createClient = () => {
 
   return createClientIn<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_SERVICE!,
   );
 }
